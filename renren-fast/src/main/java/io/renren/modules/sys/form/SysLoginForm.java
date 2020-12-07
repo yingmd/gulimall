d3d0b32f0ad2eb4045d2f50b0b37715cb9a4ada3
yingmd <yingmd@163.com>
2020-12-07 10:46:36 +0800

    111

---
+++ renren-fast/src/main/java/io/renren/modules/sys/form/SysLoginForm.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2016-2019 人人开源 All rights reserved.
+ *
+ * https://www.renren.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package io.renren.modules.sys.form;
+
+import lombok.Data;
+
+/**
+ * 登录表单
+ *
+ * @author Mark sunlightcs@gmail.com
+ */
+@Data
+public class SysLoginForm {
+    private String username;
+    private String password;
+    private String captcha;
+    private String uuid;
+
+
+    @Override
+    public String toString() {
+        return "SysLoginForm{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", captcha='" + captcha + '\'' +
+                ", uuid='" + uuid + '\'' +
+                '}';
+    }
+}
